d17a14e706af93118c6e6812bba6041e6f109164
Stuart Lynn <phy2sll@gmail.com>
2021-02-14 22:43:33 +0000

    parsing audio player requests for filename

--- Dockerfile
+++ Dockerfile
@@ -3,16 +3,19 @@ FROM $BUILD_FROM
 
 ENV LANG C.UTF-8
 
+WORKDIR /home
+
 # install dependencies
 RUN apk update && \
     apk add python3 py3-pip py3-setuptools \
     alsa-utils alsa-utils-doc alsa-lib alsaconf \
     alsa-plugins-pulse ffmpeg ffmpeg-dev
-COPY requirements.txt /
+ADD requirements.txt ./
 RUN pip3 install -r requirements.txt
 
 # Copy data for add-on
-COPY media/ /media/
-COPY app.py /
+RUN mkdir -p /home/media
+COPY media/ ./media/
+ADD app.py ./
 
 CMD [ "gunicorn", "--bind", "0.0.0.0:8099", "app:app" ]
--- app.py
+++ app.py
@@ -1,11 +1,13 @@
-from flask import Flask
+from glob import glob
+from flask import Flask, request
 from pydub import AudioSegment
 from pydub.playback import play
 
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-    song = AudioSegment.from_mp3("/media/bell.mp3")
-    play(song + 24)
+@app.route('/play')
+def play_file():
+    filename = request.args.get("filename")
+    audio_segment = AudioSegment.from_file(f"./media/{filename}", filename.split(".")[-1])
+    play(audio_segment + 24)
     return 'Ding, dong!'
