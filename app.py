c6e68d48aa884430f7135a1297eb4e0ce8267068
Kamil Kostrzewski <kamilkostrzewski0@gmail.com>
2022-05-21 16:34:37 +0200

    Changed code to keep music file "bell.mp3" in mem
    Changed code to keep music file "bell.mp3" in memory, that is now much faster

--- app.py
+++ app.py
@@ -3,11 +3,12 @@ from flask import Flask, request
 from pydub import AudioSegment
 from pydub.playback import play
 
+filename = "bell.mp3"
+audio_segment = AudioSegment.from_file(f"./media/{filename}", filename.split(".")[-1])
+
 app = Flask(__name__)
 
 @app.route('/play')
 def play_file():
-    filename = request.args.get("filename")
-    audio_segment = AudioSegment.from_file(f"./media/{filename}", filename.split(".")[-1])
     play(audio_segment + 24)
     return 'Ding, dong!'
